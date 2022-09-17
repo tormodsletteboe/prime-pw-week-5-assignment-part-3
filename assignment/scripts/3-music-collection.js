de7cbc339f9620955811143740c43c8c1693c287
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-09-17 13:30:18 -0500

    created var collection and the function addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection(title,artist,yearPublished){
+    let album = {
+        title:title,
+        artist:artist,
+        yearPublished
+    };
+    collection.push(album);
+    return album;
+
+}// end addToCollection
+
