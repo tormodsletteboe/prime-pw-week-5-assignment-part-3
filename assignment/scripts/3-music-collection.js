0770d74474676bcfcd778234068e9290208db592
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-09-17 13:43:05 -0500

    tested the addToCollection by adding 6 albums and loggin the collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,4 +11,12 @@ function addToCollection(title,artist,yearPublished){
     return album;
 
 }// end addToCollection
+console.log('test addToCollection');
 
+console.log(addToCollection('Sabbath Bloody Sabbath','Black Sabbath',1973));
+console.log(addToCollection('Reload','Metallica',1997));
+console.log(addToCollection('Ring Ring','Abba',1973));
+console.log(addToCollection('And Its still Alright','Nathaniel Rateliff',2020));
+console.log(addToCollection('If I Know Me','Morgan Wallen',2018));
+console.log(addToCollection('Racine Carree','Stromae',2015));
+console.log(collection);
