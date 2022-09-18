e5eb4e2ac98c23ded29a55ba5f0c123078f65f22
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-09-18 12:50:41 -0500

    added stretch goal tracks. updated addToCollecdtion, search and showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,11 +1,13 @@
 console.log('***** Music Collection *****')
 let collection = [];
 
-function addToCollection(title,artist,yearPublished){
+
+function addToCollection(title,artist,yearPublished,tracks){
     let album = {
         title:title,
         artist:artist,
-        yearPublished
+        yearPublished: yearPublished,
+        tracks:tracks
     };
     collection.push(album);
     return album;
@@ -15,8 +17,11 @@ function addToCollection(title,artist,yearPublished){
 function showCollection(argArray){
     console.log(argArray.length);
     for(let i=0;i<argArray.length;i++){
-        console.log(argArray[i].title+' by '+ argArray[i].artist+', published in '+argArray[i].yearPublished);
-    }//end for loop
+        console.log(argArray[i].title+' by '+ argArray[i].artist+', published in '+argArray[i].yearPublished+':');
+        console.log(argArray[i]['tracks']);
+        
+        
+    }//end for i loop
 }//end showCollection
 
 function findByArtist(strArtist){
@@ -36,10 +41,17 @@ function search(searchCriteria){
     }
     let artistToSearchFor = searchCriteria.artist;
     let yearToSearchFor = searchCriteria.yearPublished;
+    let trackName = searchCriteria.trackName;
     let foundAlbums=[];
     for(let i =0;i<collection.length;i++){
         if(collection[i].artist==artistToSearchFor && collection[i].yearPublished==yearToSearchFor){
-            foundAlbums.push(collection[i]);
+            //console.log(collection[i]['tracks']);
+            for(let j =0;j<collection[i]['tracks'].length;j++){
+                if(collection[i]['tracks'][j].name==trackName){
+                    foundAlbums.push(collection[i]);
+                }
+            }
+            
         }
     }//end for loop
     return foundAlbums;
@@ -47,12 +59,65 @@ function search(searchCriteria){
 }//end search
 
 console.log('test addToCollection');
-console.log(addToCollection('Sabbath Bloody Sabbath','Black Sabbath',1973));
-console.log(addToCollection('Reload','Metallica',1997));
-console.log(addToCollection('Master of Puppets','Metallica',1986));
-console.log(addToCollection('And Its still Alright','Nathaniel Rateliff',2020));
-console.log(addToCollection('If I Know Me','Morgan Wallen',2018));
-console.log(addToCollection('Racine Carree','Stromae',2015));
+let sabtracks =[
+    {name: "Sabbath Bloody Sabbath",
+    duration:'5:45'},
+    {name: "A National Acrobat",
+     duration: '6:15'},
+     {name: 'Fluff',
+     duration: '4:10'}
+];
+console.log(addToCollection('Sabbath Bloody Sabbath','Black Sabbath',1973,sabtracks));
+
+let mettracks =[
+    {name: "Fuel",
+    duration:'4:29'},
+    {name: "The Memory Remains",
+     duration: '4:38'},
+     {name: 'Devils Dance',
+     duration: '5:18'}
+];
+console.log(addToCollection('Reload','Metallica',1997,mettracks));
+
+let met2tracks =[
+    {name: "Battery",
+    duration:'5:12'},
+    {name: "Master of Puppets",
+     duration: '8:35'},
+     {name: 'The Thing That Should Not Be',
+     duration: '6:36'}
+];
+console.log(addToCollection('Master of Puppets','Metallica',1986,met2tracks));
+
+let nattracks =[
+    {name: "What a Drag",
+    duration:'3:35'},
+    {name: "And Its Still Alright",
+     duration: '3:56'},
+     {name: 'All or Nothing',
+     duration: '5:02'}
+];
+console.log(addToCollection('And Its still Alright','Nathaniel Rateliff',2020,nattracks));
+
+let morgtracks =[
+    {name: "Up Down",
+    duration:'3:17'},
+    {name: "Happy Hour",
+     duration: '3:20'},
+     {name: 'Had Me By Halftime',
+     duration: '2:52'}
+];
+console.log(addToCollection('If I Know Me','Morgan Wallen',2018,morgtracks));
+
+let stromtracks =[
+    {name: "Ta fete",
+    duration:'2:56'},
+    {name: "Papaoutai",
+     duration: '3:52'},
+     {name: 'batard',
+     duration: '3:29'}
+];
+console.log(addToCollection('Racine Carree','Stromae',2015,stromtracks));
 console.log(collection);
 console.log('------')
 console.log('test showCollection');
@@ -69,9 +134,10 @@ console.log('-------');
 console.log('test search function');
 let searchObj ={
     artist: 'Metallica',
-    yearPublished: 1986
+    yearPublished: 1986,
+    trackName: 'Battery'
 };
-console.log('search for Metallica 1986');
+console.log('search for Metallica 1986, Battery');
 console.log(search(searchObj));
 console.log('test with missing search object');
 console.log(search());
